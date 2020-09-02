e4119737b613ef3b13dcbe16c303fcb2c33af651
lzl32947@gmail.com <githublzl980223>
2020-09-02 09:26:58 +0800

    feat(Config): Add config for max waiting time
    1.Add config for max waiting time, default is 300(5min).
    
    BREAKING CHANGE: No

--- global_configs.py
+++ global_configs.py
@@ -10,12 +10,14 @@ full_image = None
 special_event_image = None
 xp_confirm_image = None
 
+# This version is used in Laptop
 Alberta_floating_throwing_time = 0.2
 Alberta_floating_after_throwing_waiting_time = 2
-Alberta_floating_straining_line_time = 3
+Alberta_floating_straining_line_time = 3.2
 Alberta_floating_lift_rod_time = 0.8
-Alberta_floating_lift_fish_time = 3
+Alberta_floating_lift_fish_time = 4
 Alberta_inner_wait_time = 2
+Alberta_max_waiting_time = 300
 
 # 47,126,150,44 (x,y,w,h) for digit of repo (left-top)
 # 257,527,200,50 (x,y,w,h) for special event confirm button
